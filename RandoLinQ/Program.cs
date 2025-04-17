b64fd8952732146d844543950366a3ea4c32845c
Chloé Chiarlini <chiarlinipro@gmail.com>
2025-04-17 15:54:20 +0200

    amelioration de la syntaxes en requete

--- RandoLinQ/Program.cs
+++ RandoLinQ/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using RandoLinQ;
 
 string json = File.ReadAllText(@$"{Directory.GetCurrentDirectory()}/Data/randonnees-velos.json");
-
 var randos = JsonConvert.DeserializeObject<List<Trek>>(json) ?? new List<Trek>();
 
 foreach (var r in randos)
@@ -16,9 +15,9 @@ foreach (var r in randos)
 }
 
 //1. Rechercher les randos avec une distance > 20 km :
-var resultatsDistance = randos
-    .Where(r => r.distance_km > 20)
-    .ToList();
+var resultatsDistance = from r in randos
+                        where r.distance_km > 20
+                        select r;
 
 Console.WriteLine("Randonnées de plus de 20 km: ");
 foreach (var r in resultatsDistance)
@@ -26,10 +25,10 @@ foreach (var r in resultatsDistance)
     Console.WriteLine($"{r.nom} - {r.distance_km} km - Difficulté: {r.difficulte}");
 }
 
-//3. Trier les randos par distance croissante :
-var resultatsDistanceCroissante = randos
-    .OrderBy(r => r.distance_km)
-    .ToList();
+//2. Trier les randos par distance croissante :
+var resultatsDistanceCroissante = from r in randos
+                                  orderby r.distance_km
+                                  select r;
 
 Console.WriteLine("Randonnées triées par distance (croissant): ");
 foreach (var r in resultatsDistanceCroissante)
@@ -37,20 +36,10 @@ foreach (var r in resultatsDistanceCroissante)
     Console.WriteLine($"{r.nom} - {r.distance_km} km");
 }
 
-//4. Trier par date :
-var triDate = randos
-    .OrderBy(r => r.date)
-    .ToList();
-
-Console.WriteLine("Randonnées triées par date : ");
-foreach (var r in triDate)
-{
-    Console.WriteLine($"{r.nom} - {r.date} - {r.distance_km} km");
-}
-
-//5. Grouper par difficulté :
-var groupParDifficulte = randos
-    .GroupBy(r => r.difficulte);
+//3. Grouper par difficulté :
+var groupParDifficulte = from r in randos
+                         group r by r.difficulte into g
+                         select g;
 
 Console.WriteLine("Randonnées groupées par difficulté :");
 foreach (var groupe in groupParDifficulte)
